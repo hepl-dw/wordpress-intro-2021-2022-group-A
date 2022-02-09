fe33768b3faa3d64fcc20a8b1af5fbfea5fa0cce
Toon Van den Bos <toon@whitecube.be>
2022-02-09 17:34:15 +0100

    Added header & footer templates

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?= get_bloginfo('name'); ?></h1>
-    <p><?= get_bloginfo('description'); ?></p>
-</body>
-</html>
+<?php get_header(); ?>
+
+<!-- le contenu de mon template -->
+
+<?php get_footer(); ?>
