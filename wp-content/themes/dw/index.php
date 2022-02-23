33c927b92f9fd0dcb52661378e1b97b444fbaeca
Toon Van den Bos <toon@whitecube.be>
2022-02-23 15:05:48 +0100

    Added ACF fields for trip post type

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -35,7 +35,9 @@
                 <div class="trip__card">
                     <header class="trip__head">
                         <h3 class="trip__title"><?= get_the_title(); ?></h3>
-                        <p class="trip__date">Le <time class="trip__time" datetime="">xx/xx/xxxx</time></p>
+                        <p class="trip__date"><time class="trip__time" datetime="<?= date('c', strtotime(get_field('departure_date', false, false))); ?>">
+                            <?= ucfirst(date_i18n('F, Y', strtotime(get_field('departure_date', false, false)))); ?>
+                        </time></p>
                     </header>
                     <figure class="trip__fig">
                         <?= get_the_post_thumbnail(null, 'medium_large', ['class' => 'trip__thumb']); ?>
