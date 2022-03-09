d0ecac161a49a6c1488667bfd4cbcbed245d3765
Toon Van den Bos <toon@whitecube.be>
2022-03-09 11:53:46 +0100

    Added object-oriented (MVC) menu generator

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -2,6 +2,7 @@
 
 // Charger les fichiers nécessaires
 require_once(__DIR__ . '/Menus/PrimaryMenuWalker.php');
+require_once(__DIR__ . '/Menus/PrimaryMenuItem.php');
 
 // Désactiver l'éditeur "Gutenberg" de Wordpress
 add_filter('use_block_editor_for_post', '__return_false');
@@ -40,5 +41,46 @@ function dw_get_trips($count = 20)
 }
 
 // Enregistrer les zones de menus
+
 register_nav_menu('primary', 'Navigation principale (haut de page)');
 register_nav_menu('footer', 'Navigation de pied de page');
+
+// Fonction pour récupérer les éléments d'un menu sous forme d'un tableau d'objets
+
+function dw_get_menu_items($location)
+{
+    $items = [];
+
+    // Récupérer le menu Wordpress pour $location
+    $locations = get_nav_menu_locations();
+
+    if($locations[$location] ?? false) {
+        $menu = $locations[$location];
+
+        // Récupérer tous les éléments du menu récupéré
+        $posts = wp_get_nav_menu_items($menu);
+
+        // Formater chaque élément dans une instance de classe personnalisée
+        // Boucler sur chaque $post
+        foreach($posts as $post) {
+            // Transformer le WP_Post en une instance de notre classe personnalisée
+            $item = new PrimaryMenuItem($post);
+
+            // Ajouter cette instance à $items ou à l'item parent si sous-menu
+            if($item->isSubItem()) {
+                // Ajouter $item comme "enfant" de l'item parent.
+                foreach($items as $parent) {
+                    if($parent->isParentFor($item)) {
+                        $parent->addSubItem($item);
+                    }
+                }
+            } else {
+                // Ajouter au tableau d'éléments de niveau 0.
+                $items[] = $item;
+            }
+        }
+    }
+
+    // Retourner un tableau d'éléments du menu formatés
+    return $items;
+}
--- wp-content/themes/dw/header.php
+++ wp-content/themes/dw/header.php
@@ -12,12 +12,39 @@
 
         <nav class="header__nav nav">
             <h2 class="nav__title">Navigation principale</h2>
-            <?php wp_nav_menu([
-                'menu' => 'primary',
-                'container_class' => 'nav__container',
-                'menu_class' => 'nav__links',
-                'menu_id' => 'navigation',
-                'walker' => new PrimaryMenuWalker(),
-            ]); ?>
+            <?php // wp_nav_menu([
+            //     'menu' => 'primary',
+            //     'container_class' => 'nav__container',
+            //     'menu_class' => 'nav__links',
+            //     'menu_id' => 'navigation',
+            //     'walker' => new PrimaryMenuWalker(),
+            // ]); ?>
+
+            <ul class="nav__container">
+                <?php foreach(dw_get_menu_items('primary') as $link): ?>
+                <li class="<?= $link->getBemClasses('nav__item'); ?>">
+                    <a  href="<?= $link->url; ?>"
+                        <?= $link->title ? ' title="' . $link->title . '"' : ''; ?>
+                        class="nav__link"
+                    >
+                        <?= $link->label; ?>
+                    </a>
+                    <?php if($link->hasSubItems()): ?>
+                        <ul class="nav__subcontainer">
+                            <?php foreach($link->subitems as $sub): ?>
+                            <li class="<?= $sub->getBemClasses('nav__subitem'); ?>">
+                                <a  href="<?= $sub->url; ?>"
+                                    <?= $sub->title ? ' title="' . $sub->title . '"' : ''; ?>
+                                    class="nav__link"
+                                >
+                                    <?= $sub->label; ?>
+                                </a>
+                            </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+                </li>
+                <?php endforeach; ?>
+            </ul>
         </nav>
     </header>
